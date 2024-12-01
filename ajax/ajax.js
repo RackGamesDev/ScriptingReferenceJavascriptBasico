132d5f2997cbd070988a08864aad11420f8222c8
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 00:37:13 +0100

    Retocar

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,17 +1,17 @@
-//ajax es una api que comunica navegador y servidor mandando y recibiendo archivos xml y json
+//Ajax es una api que comunica navegador y servidor mandando y recibiendo archivos xml y json
 
 
 //xml http request (xhr)
-(() => {//encapsulando esta parte para que no colisione con el resto del codigo
+(() => {//Encapsulando esta parte para que no colisione con el resto del codigo
     const xhr = new XMLHttpRequest();//creando un objeto xhr
     const $xhr = document.getElementById("xhr");
     const $fragment = document.createDocumentFragment();
 
-    xhr.addEventListener("readystatechange", (e) => {//esto se usara cuando el xhr cambie (0=unitialized 1=loading 2=loaded 3=interactive 4=complete)
-        if(xhr.readyState !== 4) return;//si pones esto, solo ejecutara este codigo cuando el xhr este completado y listo
-        if(xhr.status >= 200 && xhr.status < 300){//comprueba que todo haya ido bien, entonces ya se puede ejecutar el codigo cuando este listo
-            //console.log(xhr.responseText);//el json/xml como un string
-            let json = JSON.parse(xhr.responseText);//parseando el json
+    xhr.addEventListener("readystatechange", (e) => {//Esto se usara cuando el xhr cambie (0=unitialized 1=loading 2=loaded 3=interactive 4=complete)
+        if(xhr.readyState !== 4) return;//Si pones esto, solo ejecutara este codigo cuando el xhr este completado y listo
+        if(xhr.status >= 200 && xhr.status < 300){//Comprueba que todo haya ido bien, entonces ya se puede ejecutar el codigo cuando este listo
+            //console.log(xhr.responseText);//El json/xml como un string
+            let json = JSON.parse(xhr.responseText);//Parseando el json
 
             json.forEach(el => {
                 const $li = document.createElement("li");
@@ -23,21 +23,21 @@
             xhr.innerHTML = "error = " + (xhr.status || "):");
         }
     });
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");//para volcar el contenido de un json/xml de algun servidor se usa el GET y la url del archivo (tambien se puede usar un archvo de aqui como ruta)
-    xhr.send();//paso final, una vez recibido el contenido
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");//Para volcar el contenido de un json/xml de algun servidor se usa el GET y la url del archivo (tambien se puede usar un archvo de aqui como ruta)
+    xhr.send();//Paso final, una vez recibido el contenido
 })();
 
 
-//fetch
+//Fetch
 (() => {
     const $fetch = document.getElementById("fetch");
     const $fragment = document.createDocumentFragment();
 
-    fetch("https://jsonplaceholder.typicode.com/users")//poner la url del json/xml o archivo local
+    fetch("https://jsonplaceholder.typicode.com/users")//Poner la url del json/xml o archivo local
     .then(res => {
-        return res.ok ? res.json() : Promise.reject();//convierte la respuesta a objeto json, tambien esta .text() para string y .blob() para imagenes especiales
+        return res.ok ? res.json() : Promise.reject();//Convierte la respuesta a objeto json, tambien esta .text() para string y .blob() para imagenes especiales
     })
-    .then(son => {//si no hay errores:
+    .then(son => {//Si no hay errores:
         son.forEach((el) => {
             const $li = document.createElement("li");
             $li.innerHTML = "nombre= " + el.name + "   ciudad=" + el.address.city;
@@ -45,13 +45,13 @@
         });
         $fetch.appendChild($fragment);
     })
-    .catch(err => {//si hay algun error:
+    .catch(err => {//Si hay algun error:
         console.log(err);
         $fetch.innerHTML = "error " + err.status;
     })
-    .finally(()=>{ });//haya error o no
+    .finally(()=>{ });//Haya error o no
 })();
-//fetch con async await
+//Fetch con async await
 (() => {
     const $fetchAsync = document.getElementById("fetch-async");
     const $fragment = document.createDocumentFragment();
@@ -60,7 +60,7 @@
         try{
             let res = await fetch("https://jsonplaceholder.typicode.com/users");
             let json = await res.json();
-            if(!res.ok){//sera true si no hay problemas
+            if(!res.ok){//Sera true si no hay problemas
                 throw {status: res.status, statusText: res.statusText}
             }
             json.forEach((el) =>{
